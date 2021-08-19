9db19c4959bd404e1fdd477136c488a2a43c0d60
Aswan Abidin <aswan.abidin@kitabisa.com>
2021-08-19 23:01:21 +0800

    Initial Commit

---
+++ learnnewsapi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learnnewsapi
+//
+//  Created by Aswan Abidin on 19/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learnnewsapi/learnnewsapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnnewsapiApp.swift
+//  learnnewsapi
+//
+//  Created by Aswan Abidin on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct learnnewsapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
